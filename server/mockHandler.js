973d0966112de03c9a1c737df40eb954d783d3f0
frank-deng <13382084679@163.com>
2021-09-27 20:11:33 +0800

    mock server updated

--- server/mockHandler.js
+++ server/mockHandler.js
@@ -17,10 +17,10 @@ module.exports=class extends httpServer{
                     }
                 }
             }
-            response.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-            response.setHeader('Access-Control-Allow-Origin', '*');
-            response.setHeader('Access-Control-Allow-Headers','content-type');
             if(!matched){
+                response.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+                response.setHeader('Access-Control-Allow-Origin', '*');
+                response.setHeader('Access-Control-Allow-Headers','content-type');
                 response.setHeader('content-type', 'text/plain; charset=utf-8');
                 response.writeHead(404);
                 response.end('404 Not Found');
@@ -28,9 +28,12 @@ module.exports=class extends httpServer{
             }
             // response 对象有一个方法：write 可以用来给客户端发送响应数据
             // write 可以使用多次，但是最后一定要使用 end 来结束响应，否则客户端会一直等待
+            //根据返回类型决定返回内容
+            response.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+            response.setHeader('Access-Control-Allow-Origin', '*');
+            response.setHeader('Access-Control-Allow-Headers','content-type');
             response.setHeader('content-type', 'application/json; charset=utf-8');
-            response.write(JSON.stringify(matched,null,2));
-            response.end()
+            response.end(matched.resp_content);
         }catch(e){
             console.error(e);
         }
